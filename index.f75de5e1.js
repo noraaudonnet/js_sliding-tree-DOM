6245d6dfd066bbb7b552b2994ce14adbcb502005
Nora Audonnet <noraaudonnet@gmail.com>
2024-06-13 22:15:58 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const listItems = [
+    ...document.querySelectorAll("li")
+];
+const headers = listItems.filter((item)=>item.querySelector("ul"));
+headers.forEach((header)=>{
+    const span = document.createElement("span");
+    header.prepend(span);
+    span.append(header.childNodes[1]);
+    span.addEventListener("click", (e)=>{
+        const ul = header.querySelector("ul");
+        ul.style.display = ul.style.display === "none" ? "block" : "none";
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
